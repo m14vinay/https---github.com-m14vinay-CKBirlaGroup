53a45112782440154a3880f31965c0ac73545125
m14vinay <singhvinay182@gmail.com>
2026-03-24 16:22:24 +0530

    Vendor Form Design Pushed

---
+++ src/webparts/vendorMappingDetails/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IVendorMappingDetailsWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'VendorMappingDetailsWebPartStrings' {
+  const strings: IVendorMappingDetailsWebPartStrings;
+  export = strings;
+}
