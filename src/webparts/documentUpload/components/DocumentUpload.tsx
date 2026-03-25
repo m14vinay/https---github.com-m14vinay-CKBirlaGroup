c15137a9df96a99c84718e6b47891639eaafb745
m14vinay <singhvinay182@gmail.com>
2026-03-25 13:34:54 +0530

    Document Upload and Document View Design Done

--- src/webparts/documentUpload/components/DocumentUpload.tsx
+++ src/webparts/documentUpload/components/DocumentUpload.tsx
@@ -1,43 +1,201 @@
 import * as React from 'react';
 import styles from './DocumentUpload.module.scss';
-import type { IDocumentUploadProps } from './IDocumentUploadProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import { IDocumentUploadProps } from './IDocumentUploadProps';
+import { SPHttpClient } from '@microsoft/sp-http';
 
-export default class DocumentUpload extends React.Component<IDocumentUploadProps> {
+interface IState {
+  TypeofDocument:string;
+  NameofDocument: string;
+  BillNumber: string;
+  BillDate: Date;
+  vendorName: string;
+  BillAmount: number;
+  Remarks: string;
+  files: FileList | null;
+}
+export default class DocumentUpload extends React.Component<IDocumentUploadProps, IState> {
+
+  constructor(props: IDocumentUploadProps) {
+    super(props);
+
+    this.state = {
+      TypeofDocument:'',
+      NameofDocument: '',
+      BillNumber: '',
+      BillDate: new Date(),
+      vendorName: '',
+      BillAmount : 0,
+      Remarks: '',
+      files:  null
+    };
+  }
+
+  private handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+    const { name, value } = e.target;
+    this.setState({ ...this.state, [name]: value });
+  };
+
+ private getRequestDetails = async (requestNo: string) => {
+ 
+  const url = `${this.props.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('QuotationApproval')/items?$filter=RequestNo eq '${requestNo}'`;
+
+    console.log("URL:",url)  
+  const response = await this.props.context.spHttpClient.get(
+    url,
+    SPHttpClient.configurations.v1
+  );
+  
+ const data = await response.json();
+
+  if (data.value.length > 0) {
+    this.setState({
+      TypeofDocument: data.value[0].ProjectTitle,
+      NameofDocument: data.value[0].ProjectDescription,
+      BillNumber: data.value[0].ProjectTitle,
+      BillDate: new Date(data.value[0].Created),
+      vendorName: data.value[0].VendorName,
+      BillAmount: data.value[0].RemainingAmount,
+      Remarks: data.value[0].Department      
+    });
+  } else {
+   
+    this.setState({
+      TypeofDocument: '',
+      NameofDocument: '',
+      BillNumber: '',
+      BillDate: new Date(),
+      vendorName: '',
+      BillAmount: 0,
+      Remarks:''
+    });
+  }
+};
+ 
+private handleRequestNoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const value = e.target.value;
+
+  this.setState({ NameofDocument: value });
+
+ // optional
+    this.getRequestDetails(value);
+  
+};
+
+  private handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    this.setState({ files: e.target.files });
+  };
+  private saveData = async () => {
+
+  const url = `${this.props.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('VendorMapping')/items?$format=json`;
+
+  const body = {
+    NameofDocument: this.state.NameofDocument,
+    TypeofDocument: this.state.TypeofDocument,
+    BillNumber: this.state.BillNumber,
+    BillDate: this.state.BillDate.toISOString(),
+    vendorName: this.state.vendorName,
+    BillAmount: this.state.BillAmount,
+    Remarks: this.state.Remarks
+     };
+  
+  const response = await this.props.context.spHttpClient.post(
+    url,SPHttpClient.configurations.v1,
+   {
+      headers: {
+        "Accept": "application/json;odata=nometadata",
+        "Content-Type": "application/json;odata=nometadata"
+      },
+      body: JSON.stringify(body)
+    }
+  );
+   const result = await response.json();
+  console.log("Response:", result);
+
+   if (response.ok) {
+    alert("Data Saved Successfully ✅");
+  } else {
+    alert("Error saving data ❌");
+  }
+};
+  
+  
+  private handleSubmit = () => {
+    console.log("Form Data:", this.state);
+    alert("Form Submitted");
+  };
+
+  private handleSave = () => {
+    console.log("Saved Data:", this.state);
+    alert("Saved");
+  };
+
   public render(): React.ReactElement<IDocumentUploadProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
 
     return (
-      <section className={`${styles.documentUpload} ${hasTeamsContext ? styles.teams : ''}`}>
-        <div className={styles.welcome}>
-          <img alt="" src={isDarkTheme ? require('../assets/welcome-dark.png') : require('../assets/welcome-light.png')} className={styles.welcomeImage} />
-          <h2>Well done, {escape(userDisplayName)}!</h2>
-          <div>{environmentMessage}</div>
-          <div>Web part property value: <strong>{escape(description)}</strong></div>
+      <div className={styles.container}>
+
+        {/* LEFT FORM */}
+        <div className={styles.leftPanel}>
+          <h2>Upload New Document</h2>
+          <h4>My Document List/ Upload New Document</h4>
+
+          <label>Type of Document</label>
+          <input value={this.state.TypeofDocument}  onChange={this.handleChange}  />
+
+          <label>Name of Document</label>
+          <input name="NameofDocument" value={this.state.NameofDocument}  />
+
+          <label>Vendor Name</label>
+          <input name="vendorName" value={this.state.vendorName}  />
+
+          <label>Bill Number</label>
+          <input name="BillNumber" value={this.state.BillNumber}   >
+          </input>
+
+          <label>Bill Date</label>
+          <input name="BillDate" type="date" value={this.state.BillDate.toISOString().split('T')[0]}  />
+
+          <label>Bill Amount</label>
+          <input name="BillAmount" value={this.state.BillAmount}  />
+
+          <label>Remarks</label>
+          <input name="Remarks" value={this.state.Remarks}   >
+          </input>
+
+          <label>Attach Documents</label>
+          <input type="file" multiple onChange={this.handleFileChange} />
+
+          {/* Buttons */}
+          <div className={styles.buttonGroup}>
+            <button className={styles.submitBtn} onClick={this.handleSubmit}>Submit</button>
+            <button className={styles.cancelBtn}>Cancel</button>
+          </div>
         </div>
-        <div>
-          <h3>Welcome to SharePoint Framework!</h3>
-          <p>
-            The SharePoint Framework (SPFx) is a extensibility model for Microsoft Viva, Microsoft Teams and SharePoint. It&#39;s the easiest way to extend Microsoft 365 with automatic Single Sign On, automatic hosting and industry standard tooling.
-          </p>
-          <h4>Learn more about SPFx development:</h4>
-          <ul className={styles.links}>
-            <li><a href="https://aka.ms/spfx" target="_blank" rel="noreferrer">SharePoint Framework Overview</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-graph" target="_blank" rel="noreferrer">Use Microsoft Graph in your solution</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-teams" target="_blank" rel="noreferrer">Build for Microsoft Teams using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-viva" target="_blank" rel="noreferrer">Build for Microsoft Viva Connections using SharePoint Framework</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-store" target="_blank" rel="noreferrer">Publish SharePoint Framework applications to the marketplace</a></li>
-            <li><a href="https://aka.ms/spfx-yeoman-api" target="_blank" rel="noreferrer">SharePoint Framework API reference</a></li>
-            <li><a href="https://aka.ms/m365pnp" target="_blank" rel="noreferrer">Microsoft 365 Developer Community</a></li>
-          </ul>
+
+        {/* RIGHT PANEL */}
+        <div className={styles.rightPanel}>
+          {/* Templates */}
+          <div className={styles.card}>
+            <h4>Templates</h4>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </div>
+
+          {/* Guidelines */}
+          <div className={styles.card}>
+            <h4>Important Guidelines</h4>
+            <ol>
+              <li>Select approval path carefully.</li>
+              <li>Use project reference if needed.</li>
+              <li>Attach all documents (Max 25 MB).</li>
+              <li>Avoid special characters in file names.</li>
+            </ol>
+          </div>
         </div>
-      </section>
+      </div>
     );
   }
 }
--- src/webparts/documentView/DocumentViewWebPart.ts
+++ src/webparts/documentView/DocumentViewWebPart.ts
@@ -29,7 +29,8 @@ export default class DocumentViewWebPart extends BaseClientSideWebPart<IDocument
         isDarkTheme: this._isDarkTheme,
         environmentMessage: this._environmentMessage,
         hasTeamsContext: !!this.context.sdks.microsoftTeams,
-        userDisplayName: this.context.pageContext.user.displayName
+        userDisplayName: this.context.pageContext.user.displayName,
+        context: this.context
       }
     );
 
