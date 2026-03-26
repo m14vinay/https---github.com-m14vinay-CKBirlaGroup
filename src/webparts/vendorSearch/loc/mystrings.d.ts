3101d2408f368e822a9a75babb599021f92b4c88
Atul Singh <asingh310186@gmail.com>
2026-03-26 11:04:29 +0530

    VendorSearchDesigning

---
+++ src/webparts/vendorSearch/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IVendorSearchWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'VendorSearchWebPartStrings' {
+  const strings: IVendorSearchWebPartStrings;
+  export = strings;
+}
