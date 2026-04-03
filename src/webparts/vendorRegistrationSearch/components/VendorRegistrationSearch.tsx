c09789f21c276be3c18706392ff1f7e9969a8bda
m14vinay <singhvinay182@gmail.com>
2026-04-03 13:25:23 +0530

    Vendor Registration Number

--- src/webparts/vendorRegistrationSearch/components/VendorRegistrationSearch.tsx
+++ src/webparts/vendorRegistrationSearch/components/VendorRegistrationSearch.tsx
@@ -1,21 +1,364 @@
 import * as React from 'react';
 import styles from './VendorRegistrationSearch.module.scss';
-import type { IVendorRegistrationSearchProps } from './IVendorRegistrationSearchProps';
-import { escape } from '@microsoft/sp-lodash-subset';
+import { IVendorRegistrationSearchProps } from './IVendorRegistrationSearchProps';
+import { Dropdown, Icon, IDropdownOption, Label } from '@fluentui/react';
+import SharePointService from '../service/Service';
+import { Spinner, SpinnerSize } from '@fluentui/react';
+import { useEffect, useState } from 'react';
+import {
+  createColumnHelper,
+  getCoreRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  getFilteredRowModel,
+  flexRender,
+  useReactTable,
+} from '@tanstack/react-table';
+import Table from 'react-bootstrap/Table';
+import 'bootstrap/dist/css/bootstrap.min.css';
+const VendorRegistrationSearch: React.FC<IVendorRegistrationSearchProps> = (props) => {
+  const [form, setForm] = React.useState({
+    VendorName: '',
+    ID: '',
+    BillNumber: '',
+    BillDate: '',
+    BillAmount: '',
+    Title: ''
+  });
 
-export default class VendorRegistrationSearch extends React.Component<IVendorRegistrationSearchProps> {
-  public render(): React.ReactElement<IVendorRegistrationSearchProps> {
-    const {
-      description,
-      isDarkTheme,
-      environmentMessage,
-      hasTeamsContext,
-      userDisplayName
-    } = this.props;
 
-    return (
-      <section>
-      </section>
-    );
-  }
-}
+  const [loading, setLoading] = React.useState(false);
+  const [vendorOptions, setVendorOptions] = React.useState<IDropdownOption[]>([]);
+  const [BillNumberOptions, setBillNumberOptions] = React.useState<IDropdownOption[]>([]);
+  const [BillDateOptions, setBillDateOptions] = React.useState<IDropdownOption[]>([]);
+  const [BillAmountOptions, setBillAmountOptions] = React.useState<IDropdownOption[]>([]);
+  const [TitleOptions, setTitleOptions] = React.useState<IDropdownOption[]>([]);
+  const service = new SharePointService(props.context);
+  const [search, setSearch] = useState("");
+  const [data, _setData] = useState<any[]>(() => []);
+  const [user, setUser] = useState<any>(null);
+  const [globalFilter, setGlobalFilter] = useState("");
+  const [sorting, setSorting] = useState<any>([]);
+
+  const columnHelper = createColumnHelper<any>()
+  const columns = [
+    columnHelper.accessor('ID', {
+      header: () => <span>Vendor Code</span>
+    }),    
+    columnHelper.accessor('VendorName', {
+      header: () => <span>Vendor Name</span>
+    }),
+    columnHelper.accessor('Title', {
+      header: () => 'MSME Registration Number'
+    }),
+    columnHelper.accessor('BillNumber', {
+      header: 'PAN'
+    }),
+    columnHelper.accessor('BillDate', {
+      header: 'GST',
+      cell: info =>
+        info.getValue()
+          ? new Date(info.getValue()).toLocaleDateString()
+          : ""
+    }),
+    columnHelper.accessor('BillAmount', {
+      header: 'Submitted Date'
+    }),
+    columnHelper.accessor('Created', {
+      header: 'Establishment Year',
+      cell: info => new Date(info.getValue()).toLocaleDateString()
+    }),
+    columnHelper.accessor(row => user?.Title, {
+      id: 'Author',
+      header: 'Approval History'
+    }),
+    columnHelper.display({
+      id: 'view',
+      header: 'View',
+      cell: info => (
+        <button onClick={() => handleView(info.row.original.ID)}>
+          View
+        </button>
+      )
+    })
+  ]
+  const table = useReactTable({
+    data,
+    columns,
+    getCoreRowModel: getCoreRowModel(),
+    state: {
+      globalFilter,
+      sorting,
+    },
+    onGlobalFilterChange: setGlobalFilter,
+    onSortingChange: setSorting,
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+  });
+  // Load data
+  React.useEffect(() => {
+    getUser();
+  }, []);
+  // Load the User Details
+  const getUser = async () => {
+    const data = await service.getUser();
+    if (data.Id > 0) {
+      setUser(data);
+      loadMaster(data.Id); // Load the Master Data for Dropdown based on User ID  
+    }
+  };
+  //Load the Master Data for Dropdown
+  const loadMaster = async (userId: number) => {
+    const data = await service.getMasterDocument(userId);
+    if (data && Array.isArray(data)) {
+      const BillNumberOption = data.map((item: any) => ({
+        key: item.BillNumber,
+        text: item.BillNumber
+      }));
+      const BillDateOption = data.map((item: any) => ({
+        key: item.BillDate ? new Date(item.BillDate).toLocaleDateString() : "",
+        text: item.BillDate ? new Date(item.BillDate).toLocaleDateString() : ""
+      }));
+      const BillAmountOption = data.map((item: any) => ({
+        key: item.BillAmount,
+        text: item.BillAmount
+      }));
+      const VendorOption = data.map((item: any) => ({
+        key: item.VendorName,
+        text: item.VendorName
+      }));
+      const TitleOption = data.map((item: any) => ({
+        key: item.Title,
+        text: item.Title
+      }));
+      setVendorOptions(VendorOption);
+      setBillNumberOptions(BillNumberOption);
+      setBillDateOptions(BillDateOption);
+      setBillAmountOptions(BillAmountOption);
+      setTitleOptions(TitleOption);
+    }
+  };
+  const handleAddNewDocument = () => {
+    const url = `${props.context.pageContext.web.absoluteUrl}/SitePages/DocumentUpload.aspx`;
+    window.location.assign(url);
+  };
+  const handleView = (documentId: string) => {
+    const url = `${props.context.pageContext.web.absoluteUrl}/SitePages/DocumentView.aspx?ID=${documentId}`;
+    window.location.assign(url);
+  };
+  const handlesearch = async () => {
+    _setData([]);
+    if (!form.VendorName && !form.BillAmount && !form.Title && !form.BillDate && !form.BillNumber) {
+      alert("Please select any one  fields to search");
+      return;
+    }
+    await getDatafromListByTitle(form.VendorName, form.BillAmount, form.Title, form.BillDate, form.BillNumber);
+  };
+  const getDatafromListByTitle = async (parm_vendorname: string, parm_billamount: string, parm_title: string, parm_billdate: string, parm_billnumber: string) => {
+    try {
+      setLoading(true);
+      const data = await service.getItemByTitle(parm_vendorname, parm_billamount, parm_title, parm_billdate, parm_billnumber);
+      if (data) {
+        _setData((d) => [...d.concat(data)]);
+      }
+    } catch (error) {
+      console.error(error);
+      alert("Error occurred");
+    }
+    finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h2>All Vendors
+          <span>Digiflow / All Vendor List</span>
+        </h2>
+      </div>
+      <div className={styles.searchBox}>
+        <h3>Search Vendor
+          <button className={styles.btnAdd} onClick={handleAddNewDocument}>Add New Document</button>
+        </h3>
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <div className={styles['col-md-4']}>
+              <label>Vendor Name</label>
+              <Dropdown
+                options={vendorOptions}
+                selectedKey={form.VendorName}
+                onChange={(e, option) =>
+                  setForm({ ...form, VendorName: option?.text as string, ID: option?.key as string, })
+                }
+              />
+
+            </div>
+            <div className={styles['col-md-4']}>
+              <label>GST</label>
+              <Dropdown
+                options={BillNumberOptions}
+                selectedKey={form.BillNumber}
+                onChange={(e, option) =>
+                  setForm({ ...form, BillNumber: option?.text as string, ID: option?.key as string, })
+                }
+              />
+
+            </div>
+            <div className={styles['col-md-4']}>
+              <label>PAN</label>
+              <Dropdown
+                options={BillAmountOptions}
+                selectedKey={form.BillAmount}
+                onChange={(e, option) =>
+                  setForm({ ...form, BillAmount: option?.text as string, ID: option?.key as string, })
+                }
+              />
+
+            </div>
+            <div className={styles['col-md-4']}>
+              <label>Vendor Code</label>
+              <Dropdown
+                options={BillDateOptions}
+                selectedKey={form.BillDate}
+                onChange={(e, option) =>
+                  setForm({ ...form, BillDate: option?.text as string, ID: option?.key as string, })
+                }
+              />
+
+            </div>
+            <div className={styles['col-md-4']}>
+              <label>TIN Number</label>
+              <Dropdown
+                options={TitleOptions}
+                selectedKey={form.Title}
+                onChange={(e, option) =>
+                  setForm({ ...form, Title: option?.text as string, ID: option?.key as string, })
+                }
+              />
+            </div>
+            <div className={styles['col-md-4']} style={{ display: "flex", alignItems: "flex-end", justifyContent: "flex-end" }}>
+              <button className={styles.btnSearch} onClick={handlesearch}>Search</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="p-2">
+        <div>
+          <Label style={{ display: "inline-block" }}>All Vendor List</Label>
+          <input
+            value={globalFilter ?? ""}
+            onChange={(e) => setGlobalFilter(e.target.value)}
+            placeholder="Search..."
+            style={{ marginBottom: "10px", padding: "5px", float: "right" }}
+          />
+        </div>
+        <Table striped bordered hover>
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th
+                    key={header.id}
+                    onClick={header.column.getToggleSortingHandler()}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                        header.column.columnDef.header,
+                        header.getContext(),
+                      )}
+                    {{
+                      asc: <Icon iconName='ChevronUpMed' style={{ verticalAlign: "middle", marginLeft: "5px" }} />,
+                      desc: <Icon iconName='ChevronDownMed' style={{ verticalAlign: "middle", marginLeft: "5px" }} />,
+                    }[header.column.getIsSorted() as string] ?? null}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            {table.getRowModel().rows.map((row) => (
+              <tr key={row.id}>
+                {row.getVisibleCells().map((cell) => (
+                  <td key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+
+        {/* 📄 Pagination */}
+        <div className="flex items-center gap-2">
+          <span>
+            Showing {table.getRowModel().rows.length.toLocaleString()} of{' '}
+            {table.getRowCount().toLocaleString()} Rows
+          </span>
+          <div style={{ float: "right" }} className="flex items-center gap-2">
+            <label>
+              Go to page:
+            </label>
+            <label>
+              <input
+                type="number"
+                min="1"
+                max={table.getPageCount()}
+                defaultValue={table.getState().pagination.pageIndex + 1}
+                onChange={(e) => {
+                  const page = e.target.value ? Number(e.target.value) - 1 : 0
+                  table.setPageIndex(page)
+                }}
+                className="border p-1 rounded w-16"
+              />
+            </label>
+            <button
+              className="border rounded p-1"
+              onClick={() => table.firstPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              {'<<'}
+            </button>
+            <button
+              className="border rounded p-1"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+            >
+              {'<'}
+            </button>
+            <button
+              className="border rounded p-1"
+              onClick={() => table.nextPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              {'>'}
+            </button>
+            <button
+              className="border rounded p-1"
+              onClick={() => table.lastPage()}
+              disabled={!table.getCanNextPage()}
+            >
+              {'>>'}
+            </button>
+            <span>Page size</span>
+            <select
+              value={table.getState().pagination.pageSize}
+              onChange={(e) => {
+                table.setPageSize(Number(e.target.value))
+              }}
+            >
+              {[10, 20, 30, 40, 50].map((pageSize) => (
+                <option key={pageSize} value={pageSize}>
+                  {pageSize}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+
+
+      </div>
+    </div>
+  );
+};
+export default VendorRegistrationSearch;
