aa81e4f447be554e947935fa31b2ce6aa83c1212
m14vinay <singhvinay182@gmail.com>
2026-04-28 08:42:36 +0530

    Issue Fixed

--- src/webparts/vendorRegistrationSearch/service/Service.ts
+++ src/webparts/vendorRegistrationSearch/service/Service.ts
@@ -53,7 +53,8 @@ if (parm_Pan) {
 const filterQuery = filters.length > 0
   ? `$filter=${filters.join(" or ")}`
   : "";
-  const url = `${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('${this.listname}')/items?${filterQuery}`;
+  const orderBy = `$orderby=Created desc`;
+  const url = `${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('${this.listname}')/items?${filterQuery}${filterQuery ? "&" : ""}${orderBy}`;
   const res = await this.context.spHttpClient.get(
     url,
     SPHttpClient.configurations.v1
