2758e283a11a80406668f86d87db0e7a6abffb67
m14vinay <singhvinay182@gmail.com>
2026-04-30 11:02:07 +0530

    Issue Fixed Report

--- src/webparts/requestHistoryReportNeibt/service/Service.ts
+++ src/webparts/requestHistoryReportNeibt/service/Service.ts
@@ -13,54 +13,33 @@ export default class Service {
   FromDate: string,
   ToDate: string
 ): Promise<any[]> {
+  let filters: string[] = [];
+  if (FromDate && ToDate) {
 
-  try {
+      // ✅ Convert dd-MM-yyyy → ISO safely
+      const parseDate = (dateStr: string) => {
+        const [day, month, year] = dateStr.split("-");
+        return new Date(`${year}-${month}-${day}`);
+      };
 
-    // ✅ Parse dd-MM-yyyy safely
-    const parseDate = (dateStr: string) => {
-      const [day, month, year] = dateStr.split("-");
-      return new Date(`${year}-${month}-${day}`);
-    };
+      const start = parseDate(FromDate);
+      start.setHours(0, 0, 0, 0);
 
-    const start = parseDate(FromDate);
-    start.setHours(0, 0, 0, 0);
+      const end = parseDate(ToDate);
+      end.setHours(23, 59, 59, 999);
 
-    const end = parseDate(ToDate);
-    end.setHours(23, 59, 59, 999);
+      filters.push(`Created ge datetime'${start.toISOString()}' and Created le datetime'${end.toISOString()}'`);
+    }
 
-    const camlQuery = `
-      <View>
-        <Query>
-          <Where>
-            <And>
-              <Geq>
-                <FieldRef Name='Created' />
-                <Value Type='DateTime' IncludeTimeValue='TRUE'>
-                  ${start.toISOString()}
-                </Value>
-              </Geq>
-              <Leq>
-                <FieldRef Name='Created' />
-                <Value Type='DateTime' IncludeTimeValue='TRUE'>
-                  ${end.toISOString()}
-                </Value>
-              </Leq>
-            </And>
-          </Where>
-        </Query>
-        <RowLimit>200</RowLimit>
-      </View>`;
-
-    const items = await sp.web.lists
-      .getByTitle(this.listname)
-      .getItemsByCAMLQuery({ ViewXml: camlQuery });
-
-    return items || [];
-
-  } catch (error) {
-    console.error("Error:", error);
-    return [];
-  }
+  // Combine filters
+  const filterQuery = filters.length > 0 ? `$filter=${filters.join(" or ")}` : "";
+  const url = `${this.context.pageContext.web.absoluteUrl}/_api/web/lists/getbytitle('${this.listname}')/items?${filterQuery}`;
+  const res = await this.context.spHttpClient.get(
+    url,
+    SPHttpClient.configurations.v1
+  );
+  const data = await res.json();
+  return data.value.length > 0 ? data.value: []; // Return array of results or empty array if no matches
 }
  public async getUser(): Promise<any> {
     const url = `${this.context.pageContext.web.absoluteUrl}/_api/web/currentuser`;
